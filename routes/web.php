e2b218b34f792e714e9bd4874bafe164895fee5c
Karim <72211@edu.sde.dk>
2026-04-17 11:30:36 +0200

    change home route to show users

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::inertia('/', 'RankList')->name('ranklist');
-
+Route::get('/', [UserController::class, 'index'])->name('home');
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/{user}', [UserController::class, 'view'])->name('users.view');
 Route::post('/users', [UserController::class, 'add'])->name('users.add');
