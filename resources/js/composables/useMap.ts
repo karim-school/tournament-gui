d3484fdba62cdca771195c09798c4f8c27286b3e
groovetonight <42851559+groovetonight@users.noreply.github.com>
2026-05-12 00:45:58 +0200

    fix: map marker icon not found

--- resources/js/composables/useMap.ts
+++ resources/js/composables/useMap.ts
@@ -2,6 +2,16 @@ import 'leaflet/dist/leaflet.css';
 import * as L from 'leaflet';
 import type { Ref } from 'vue';
 import type { WorldLocation } from '@/types';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+});
 
 type MapStateOptions = {
     mapId: string,
